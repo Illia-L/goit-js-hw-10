65be4350c13560a1a0225d68f3c5638bedafe843
Illia Liashko <2275040@gmail.com>
2025-02-04 22:54:15 +0200

    play

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig(({ command }) => {
     },
     root: 'src',
     build: {
-      assetsInclude: ['src/img/icon-success.svg', 'src/img/icon-error.svg'],
+      assetsInclude: ['./src/img/icon-success.svg', './src/img/icon-error.svg'],
       sourcemap: true,
       rollupOptions: {
         input: glob.sync('./src/*.html'),
